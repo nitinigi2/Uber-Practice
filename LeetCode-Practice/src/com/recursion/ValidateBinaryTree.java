0b57e69a0987a578d002679be60fa299eacba567
Nitin Singhal <Nitin_Singhal@epam.com>
2021-03-21 12:38:52 +0530

    Add validate binary tree solution

--- LeetCode-Practice/src/com/recursion/ValidateBinaryTree.java
+++ LeetCode-Practice/src/com/recursion/ValidateBinaryTree.java
@@ -1,2 +1,19 @@
-package src.com.recursion;public class ValidateBinaryTree {
+package src.com.recursion;
+//https://leetcode.com/problems/validate-binary-search-tree/submissions/
+public class ValidateBinaryTree {
+    TreeNode prev = null;
+    public boolean isValidBST(TreeNode root) {
+        if(root != null){
+            if(!isValidBST(root.left)) return false;
+            if(prev == null){
+                prev = root;
+            }else{
+                if(prev.val >= root.val) return false;
+                prev = root;
+            }
+            return isValidBST(root.right);
+        }
+        return true;
+    }
 }
+
