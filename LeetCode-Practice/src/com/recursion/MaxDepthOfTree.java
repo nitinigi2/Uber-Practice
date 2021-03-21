ae7003da9464a421281a2e62b2a46befe172872c
Nitin Singhal <Nitin_Singhal@epam.com>
2021-03-21 11:53:27 +0530

    Add max depth of tree solution

---
+++ LeetCode-Practice/src/com/recursion/MaxDepthOfTree.java
@@ -0,0 +1,19 @@
+package src.com.recursion;
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+public class MaxDepthOfTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+}
+
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+}
