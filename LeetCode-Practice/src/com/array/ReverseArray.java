8d41356be5fca63a531733d55520daf53b9feb8f
Nitin Singhal <nitinsinghal139@gmail.com>
2021-03-22 15:17:08 +0530

    Add reverse array solution

--- LeetCode-Practice/src/com/array/ReverseArray.java
+++ LeetCode-Practice/src/com/array/ReverseArray.java
@@ -1,2 +1,30 @@
-package src.com.array;public class ReverseArray {
+package src.com.array;
+
+import java.util.stream.Stream;
+
+public class ReverseArray {
+
+    public static void reverse(int[] arr) {
+        int i=0, j=arr.length-1;
+        while(i<j){
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 10, 3};
+        reverse(arr);
+    }
+
 }
