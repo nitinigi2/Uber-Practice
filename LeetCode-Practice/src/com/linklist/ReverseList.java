b4272783a2d573ac1e1bbb7b7638aae60c76d318
Nitin Singhal <nitinsinghal139@gmail.com>
2021-03-22 14:47:02 +0530

    Add reverse linked list solution

---
+++ LeetCode-Practice/src/com/linklist/ReverseList.java
@@ -0,0 +1,33 @@
+package src.com.linklist;
+
+public class ReverseList {
+    // recursive solution
+    static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+        if (head == null || head.next == null) return head;
+        SinglyLinkedListNode rev = reverse(head.next);
+        head.next.next = head;
+
+        head.next = null;
+
+        return rev;
+    }
+
+    // iterative solution
+    static SinglyLinkedListNode reverseIterative(SinglyLinkedListNode head) {
+        SinglyLinkedListNode curr = head;
+        SinglyLinkedListNode prev = null, next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+}
+
+class SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode next;
+}
