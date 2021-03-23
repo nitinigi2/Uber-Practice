8506e04459be189cb0d88ca69cd87b94896fb2f2
Nitin Singhal <nitinsinghal139@gmail.com>
2021-03-23 20:42:24 +0530

    Add path sum leetcode sulution

---
+++ LeetCode-Practice/src/com/tree/PathSum.java
@@ -0,0 +1,13 @@
+package src.com.tree;
+
+// https://leetcode.com/problems/path-sum/
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if(root == null) return false;
+
+        if(root.left == null && root.right == null && root.val - targetSum == 0) return true;
+
+        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
