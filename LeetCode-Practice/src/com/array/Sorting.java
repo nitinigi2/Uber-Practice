18a56efb6e974657093e3aa1610a9c29be3570ae
Nitin Singhal <nitinsinghal139@gmail.com>
2021-03-30 12:26:41 +0530

    Add bubble sort algorithm

--- LeetCode-Practice/src/com/array/Sorting.java
+++ LeetCode-Practice/src/com/array/Sorting.java
@@ -1,2 +1,38 @@
-package src.com.array;public class Sorting {
+package src.com.array;
+
+public class Sorting {
+    public static void bubbleSort(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+        System.out.println("Bubble sort: ");
+        printArray(arr, n);
+    }
+
+
+
+    private static void swap(int[] arr, int j, int i) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void printArray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, -1, 10, 4, 66, 3};
+        int n = arr.length;
+        bubbleSort(arr, n);
+    }
 }
+
+// -1, 2
