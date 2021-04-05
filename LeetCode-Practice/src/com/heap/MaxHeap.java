22bbd94d1ec5e47d51af7c867e713dc3904a21fc
Nitin Singhal <nitinsinghal139@gmail.com>
2021-04-05 12:30:50 +0530

    Add merge two max heaps implementation

--- LeetCode-Practice/src/com/heap/MaxHeap.java
+++ LeetCode-Practice/src/com/heap/MaxHeap.java
@@ -26,19 +26,71 @@ public class MaxHeap {
         }
         return true;
     }
+
+    public int[] mergeHeaps(int[] arr, int[] brr) {
+        int n = arr.length;
+        int m = brr.length;
+        int k = n + m;
+        int[] ans = new int[k];
+
+        for(int i=0;i<n;i++){
+            ans[i] = arr[i];
+        }
+
+        for(int i=0;i<m;i++){
+            ans[i+n] = brr[i];
+        }
+
+        for(int i=k/2;i>=0;i--){
+            heapify(ans, k, i);
+        }
+
+        return ans;
+    }
+
+    private void heapify(int[] arr, int k, int i) {
+        int max = i;
+        int left = getLeft(i);
+        int right = getRight(i);
+
+        if(left < k && arr[left] > arr[max]){
+            max = left;
+        }
+
+        if(right < k && arr[right] > arr[max]){
+            max = right;
+        }
+
+        if(i != max){
+            swap(arr, i, max);
+            heapify(arr, k, max);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
 
 class Test1{
     public static void main(String[] args) {
         MaxHeap maxHeap = new MaxHeap();
-        int arr[] = { 90, 15, 10, 7, 12, 2, 7, 3 };
+        /*int arr[] = { 90, 15, 10, 7, 12, 2, 7, 3 };
         int n = arr.length - 1;
         if (maxHeap.isMaxHeap(arr, n)) {
             System.out.println("Yes");
         }
         else {
             System.out.println("No");
+        }*/
+        int[] a = {10, 5, 6, 2};
+        int[] b = {12, 7, 9};
+        int[] ans = maxHeap.mergeHeaps(a, b);
+        for(int x : ans){
+            System.out.println(x);
         }
-
     }
 }
